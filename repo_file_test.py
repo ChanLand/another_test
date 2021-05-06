e034c384d37155758e396baa583c3b20dbe4d22b
ChanLand <amchan118@gmail.com>
2021-05-06 15:00:28 -0600

    typo that needed to be fixed

--- repo_file_test.py
+++ repo_file_test.py
@@ -17,9 +17,9 @@ Created on Thu May  6 14:37:57 2021
 # use !git status to make sure you're in the correct place
 # !git add filename
 # !git commit -m "your commit message here"
-# !git add 
-# NOTE: thought I'd have to do !git add -u origin master but that didn't work;
-# just plain !git add seemed to do the trick and the file showed up in the remote repo
+# !git push
+# NOTE: thought I'd have to do !git push -u origin master but that didn't work;
+# just plain !git push seemed to do the trick and the file showed up in the remote repo
 
 ########################
 # If you just do !git init in a project to initiate a local git repository and then, 
